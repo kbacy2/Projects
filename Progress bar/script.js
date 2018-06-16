8fc6600664672c1ded35512b5652ee5803fcbdf1
Pavel Leonov <syler222@gmail.com>
2018-06-17 02:27:19 +0300

    Sync audio

--- Progress bar/script.js
+++ Progress bar/script.js
@@ -5,42 +5,41 @@ var button = document.querySelector('.click');
 var hik = document.querySelector('.hik');
 var progressBar = document.querySelector('.my_progress');
 var gifka = document.querySelector('.gifka');
+var audio = document.querySelector('.sistema');
 
+audio.addEventListener('durationchange', function () {
 
-button.addEventListener('click', move);
-button.addEventListener('click', function () {
-    document.querySelector('.sistema').play();
+  button.addEventListener('click', function () {
+    move();
+    audio.play();
+  });
 
-});
-
-
-
-
-function move() {
+  function move() {
     var elem = document.querySelector('.my_bar');
     var width = 1;
-    var id = setInterval(frame, 280);
+    var id = setInterval(frame, audio.duration * 1000 / 100); // 1000 = 1s
     function frame () {
-        if(width>=100) {
-            clearInterval(id);
-            hik.style.display = 'block';
-            gifka.style.display = 'block';
-            progressBar.style.display = 'none';
-            button.style.display = 'none';
-            document.querySelector('.text').innerHTML = 'Пидарас найден!' + '               &#8594';
-        }
-        else{
-            width++;
-            elem.style.backgroundColor = '#4CAF50';
-            elem.style.width = width + '%';
-            document.querySelector('.label').innerHTML = width + '% Идет поиск... ';
+      if(width>=100) {
+        clearInterval(id);
+        hik.style.display = 'block';
+        gifka.style.display = 'block';
+        progressBar.style.display = 'none';
+        button.style.display = 'none';
+        document.querySelector('.text').innerHTML = 'Пидарас найден!' + '               &#8594';
+      }
+      else{
+        width++;
+        elem.style.backgroundColor = '#4CAF50';
+        elem.style.width = width + '%';
+        document.querySelector('.label').innerHTML = width + '% Идет поиск... ';
 
-        }
+      }
 
 
 
     }
 
-}
+  }
+});
 
 
