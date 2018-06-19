6c8a554ffe43ffe6a76071da15cb1f06875931d2
Ork <kbacy2@gmail.com>
2018-06-19 18:21:13 +0300

    content slider
    1) action - done

--- Restaurant maket/script.js
+++ Restaurant maket/script.js
@@ -1,63 +1,25 @@
 // CONTENT SLIDER
-
-// let arrowLeft = document.querySelector('#slider_menu_arrow_left');
-// let arrowRight = document.querySelector('#slider_menu_arrow_right');
-// let slideIndex = 1;
-// showSlides(slideIndex);
-//
-// arrowLeft.addEventListener('click', minusSlides);
-// arrowRight.addEventListener('click', plusSlides);
-//
-// function plusSlides (numberIndex){
-//     numberIndex = 1;
-//     showSlides(slideIndex += numberIndex);
-// }
-//
-// function minusSlides (numberIndex){
-//     numberIndex = 1;
-//     showSlides(slideIndex -= numberIndex);
-// }
-//
-// function currentSlide (n){
-//     showSlides(slideIndex = n);
-// }
-//
-// function showSlides (n){
-//     var i;
-//     var slides = document.querySelectorAll ('.slider_menu_item');
-//
-//     if(n > slides.length){
-//         slideIndex = 1;
-//     }
-//     if(n < 1){
-//         slideIndex = slides.length;
-//     }
-//
-//     for (i = 0; i < slides.length; i++) {
-//         slides[i].style.display = 'none';
-//     }
-//
-//
-//     slides[slideIndex-1].style.display = 'block';
-// }
-
-
-var sliderItems = document.querySelectorAll('.slider_menu_item');
 var slider = document.querySelector('.slider_menu');
-var width = 270; // ширина изображения
-var count = 6; // количество изображений
-var position = 0; // текущий сдвиг влево
 var arrowLeft = document.querySelector('#slider_menu_arrow_left');
 var arrowRight = document.querySelector('#slider_menu_arrow_right');
+var position = 0;
 
-arrowLeft.addEventListener("click", function () {
-    position = Math.min(position + width * count, 0)
+arrowRight.addEventListener('click', function () {
+    position = position -1170;
+    if (position < -2340){
+        position = 0;
+    }
+
     slider.style.marginLeft = position + 'px';
-
 });
 
-arrowRight.addEventListener("click", function () {
-    position = Math.max(position - width * count, -width * (sliderItems.length - count));
-    slider.style.marginLeft = position + 'px';
+arrowLeft.addEventListener('click', function () {
+    position = position + 1170;
+    if (position < -1170) {
+        position = 0;
+    } else if(position > 0){
+        position = -2340;
+    }
 
+    slider.style.marginLeft = position + 'px';
 });
