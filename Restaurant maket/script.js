1738629ab029877ec986a8269b5240c4b90013b6
Ork <kbacy2@gmail.com>
2018-06-20 15:52:38 +0300

    content slider
    1) action - done
    2) animation - done

--- Restaurant maket/script.js
+++ Restaurant maket/script.js
@@ -1,14 +1,25 @@
 // CONTENT SLIDER
 var slider = document.querySelector('.slider_menu');
+var sliderItem = document.querySelectorAll('.slider_menu_item');
 var arrowLeft = document.querySelector('#slider_menu_arrow_left');
 var arrowRight = document.querySelector('#slider_menu_arrow_right');
 var position = 0;
+var cubic = document.querySelector('.cubic');
 
 arrowRight.addEventListener('click', function () {
     position = position -1170;
     if (position < -2340){
         position = 0;
     }
+
+    sliderItem.forEach(function (slide) {
+        slide.classList.add('cubic');
+        setTimeout(function () {
+            slide.classList.remove('cubic');
+
+        },100);
+
+    });
 
     slider.style.marginLeft = position + 'px';
 });
@@ -20,6 +31,14 @@ arrowLeft.addEventListener('click', function () {
     } else if(position > 0){
         position = -2340;
     }
+
+    sliderItem.forEach(function (slide) {
+        slide.classList.toggle('cubic');
+        setTimeout(function () {
+            slide.classList.remove('cubic');
+
+        },100);
+    });
 
     slider.style.marginLeft = position + 'px';
 });
