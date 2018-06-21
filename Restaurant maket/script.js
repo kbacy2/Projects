dd410f911b826db8fb2f95f90551ac55f9ee0136
admin <kbacy2@gmail.com>
2018-06-21 14:00:25 +0300

    modal chiefs windows - starting

--- Restaurant maket/script.js
+++ Restaurant maket/script.js
@@ -42,3 +42,46 @@ arrowLeft.addEventListener('click', function () {
 
     slider.style.marginLeft = position + 'px';
 });
+
+
+//MODAL CHIEFS WINDOW
+
+var modalChiefOne = document.querySelector('.modal_chief__1');
+var modalChiefTwo = document.querySelector('.modal_chief__2');
+var modalChiefThree = document.querySelector('.modal_chief__3');
+var zoomClickOne = document.querySelector('.our_stuff__zoom_click-1');
+var zoomClickTwo = document.querySelector('.our_stuff__zoom_click-2');
+var zoomClickThree = document.querySelector('.our_stuff__zoom_click-3');
+var modalChiefImg = document.querySelector('.img_chief');
+//var captionChiefText = document.querySelectorAll('.caption_chiefs');
+var closeModalChiefs = document.querySelectorAll('.close_modal__chiefs');
+
+closeModalChiefs.forEach(function (close) {
+    close.addEventListener('click', function () {
+        modalChiefThree.style.display = 'none';
+        modalChiefTwo.style.display = 'none';
+        modalChiefOne.style.display = 'none';
+
+    });
+});
+
+zoomClickOne.addEventListener('click', function () {
+    modalChiefOne.style.display = 'block';
+    modalChiefImg.src;
+    //captionText.innerHTML = this.alt;
+
+});
+
+zoomClickTwo.addEventListener('click', function () {
+    modalChiefTwo.style.display = 'block';
+    modalChiefImg.src;
+    //captionText.innerHTML = this.alt;
+
+});
+
+zoomClickThree.addEventListener('click', function () {
+    modalChiefThree.style.display = 'block';
+    modalChiefImg.src;
+    //captionText.innerHTML = this.alt;
+
+});
