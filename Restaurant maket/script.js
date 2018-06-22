2b3adec64e83baa13ba92ca6c882fa13b73cf358
admin <kbacy2@gmail.com>
2018-06-22 13:48:03 +0300

    modal chiefs windows - done (css problems only)
    menu slider+animation - done

--- Restaurant maket/script.js
+++ Restaurant maket/script.js
@@ -85,3 +85,76 @@ zoomClickThree.addEventListener('click', function () {
     //captionText.innerHTML = this.alt;
 
 });
+
+//SLIDER-LIST FOOD MENU
+
+var allMainMenu = document.querySelector('.all_main__menu');
+var breakfastMainMenu = document.querySelector('.breakfast_main__menu');
+var lunchMainMenu = document.querySelector('.lunch_main__menu');
+var dinnerMainMenu = document.querySelector('.dinner_main__menu');
+var desertMainMenu = document.querySelector('.desert_main__menu');
+var kidsMainMenu = document.querySelector('.kids_main__menu');
+var mainMenu = document.querySelector('.main_dish_menu');
+
+var openAll = document.querySelector('.dish_item_all');
+var openBreakfast = document.querySelector('.dish_item_breakfast');
+var openLunch = document.querySelector('.dish_item_lunch');
+var openDinner = document.querySelector('.dish_item_dinner');
+var openDesert = document.querySelector('.dish_item_desert');
+var openKids = document.querySelector('.dish_item_kids');
+
+
+//Как сделать по уму при наведении мыши
+openAll.addEventListener('click', function () {
+    allMainMenu.style.display = 'flex';
+    breakfastMainMenu.style.display = 'none';
+    lunchMainMenu.style.display = 'none';
+    dinnerMainMenu.style.display = 'none';
+    desertMainMenu.style.display = 'none';
+    kidsMainMenu.style.display = 'none';
+});
+
+openBreakfast.addEventListener('click', function () {
+    allMainMenu.style.display = 'none';
+    breakfastMainMenu.style.display = 'flex';
+    lunchMainMenu.style.display = 'none';
+    dinnerMainMenu.style.display = 'none';
+    desertMainMenu.style.display = 'none';
+    kidsMainMenu.style.display = 'none';
+});
+
+openLunch.addEventListener('click', function () {
+    allMainMenu.style.display = 'none';
+    breakfastMainMenu.style.display = 'none';
+    lunchMainMenu.style.display = 'flex';
+    dinnerMainMenu.style.display = 'none';
+    desertMainMenu.style.display = 'none';
+    kidsMainMenu.style.display = 'none';
+});
+
+openDinner.addEventListener('click', function () {
+    allMainMenu.style.display = 'none';
+    breakfastMainMenu.style.display = 'none';
+    lunchMainMenu.style.display = 'none';
+    dinnerMainMenu.style.display = 'flex';
+    desertMainMenu.style.display = 'none';
+    kidsMainMenu.style.display = 'none';
+});
+
+openDesert.addEventListener('click', function () {
+    allMainMenu.style.display = 'none';
+    breakfastMainMenu.style.display = 'none';
+    lunchMainMenu.style.display = 'none';
+    dinnerMainMenu.style.display = 'none';
+    desertMainMenu.style.display = 'flex';
+    kidsMainMenu.style.display = 'none';
+});
+
+openKids.addEventListener('click', function () {
+    allMainMenu.style.display = 'none';
+    breakfastMainMenu.style.display = 'none';
+    lunchMainMenu.style.display = 'none';
+    dinnerMainMenu.style.display = 'none';
+    desertMainMenu.style.display = 'none';
+    kidsMainMenu.style.display = 'flex';
+});
