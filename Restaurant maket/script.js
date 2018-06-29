e004b104d2a270e89c0d72e1ab3ec3dfc683c188
admin <kbacy2@gmail.com>
2018-06-29 15:59:15 +0300

    modal chief windows

--- Restaurant maket/script.js
+++ Restaurant maket/script.js
@@ -58,46 +58,29 @@
 // -------------------------------------------------
 //MODAL CHIEFS WINDOW
 
-var modalChiefOne = document.querySelector('.modal_chief__1');
-var modalChiefTwo = document.querySelector('.modal_chief__2');
-var modalChiefThree = document.querySelector('.modal_chief__3');
-var zoomClickOne = document.querySelector('.our_stuff__zoom_click-1');
-var zoomClickTwo = document.querySelector('.our_stuff__zoom_click-2');
-var zoomClickThree = document.querySelector('.our_stuff__zoom_click-3');
-var modalChiefImg = document.querySelector('.img_chief');
-//var captionChiefText = document.querySelectorAll('.caption_chiefs');
-var closeModalChiefs = document.querySelectorAll('.close_modal__chiefs');
 
-closeModalChiefs.forEach(function (close) {
-    close.addEventListener('click', function () {
-        modalChiefThree.style.display = 'none';
-        modalChiefTwo.style.display = 'none';
-        modalChiefOne.style.display = 'none';
-        document.body.removeEventListener('wheel', preventScroll);
+
+
+var zoomButtons = document.querySelectorAll('.slider_our_stuff_zoom');
+zoomButtons.forEach(function (zoomButton) {
+    zoomButton.addEventListener('click', function (event) {
+        var zoomButtonTarget = event.target.dataset.target;
+        var showModalChief = document.querySelector('.' + zoomButtonTarget);
+        var modalChiefImg = document.querySelector('.img_chief');
+        document.body.addEventListener('wheel', preventScroll);
+        showModalChief.style.display = 'block';
+        modalChiefImg.src;
+
+        var closeModalChiefs = document.querySelectorAll('.close_modal__chiefs');
+
+        closeModalChiefs.forEach(function (close) {
+            close.addEventListener('click', function () {
+                showModalChief.style.display = 'none';
+                document.body.removeEventListener('wheel', preventScroll);
+            });
+        });
+
     });
-});
-
-zoomClickOne.addEventListener('click', function () {
-    document.body.addEventListener('wheel', preventScroll);
-    modalChiefOne.style.display = 'block';
-    modalChiefImg.src;
-    //captionText.innerHTML = this.alt;
-
-});
-
-zoomClickTwo.addEventListener('click', function () {
-    document.body.addEventListener('wheel', preventScroll);
-    modalChiefTwo.style.display = 'block';
-    modalChiefImg.src;
-    //captionText.innerHTML = this.alt;
-
-});
-
-zoomClickThree.addEventListener('click', function () {
-    document.body.addEventListener('wheel', preventScroll);
-    modalChiefThree.style.display = 'block';
-    modalChiefImg.src;
-    //captionText.innerHTML = this.alt;
 
 });
 
@@ -105,6 +88,56 @@ function preventScroll(e) {
     e.preventDefault();
     e.stopPropagation();
 }
+
+
+
+// var modalChiefOne = document.querySelector('.modal_chief__1');
+// var modalChiefTwo = document.querySelector('.modal_chief__2');
+// var modalChiefThree = document.querySelector('.modal_chief__3');
+// var zoomClickOne = document.querySelector('.our_stuff__zoom_click-1');
+// var zoomClickTwo = document.querySelector('.our_stuff__zoom_click-2');
+// var zoomClickThree = document.querySelector('.our_stuff__zoom_click-3');
+// var modalChiefImg = document.querySelector('.img_chief');
+// //var captionChiefText = document.querySelectorAll('.caption_chiefs');
+// var closeModalChiefs = document.querySelectorAll('.close_modal__chiefs');
+//
+// closeModalChiefs.forEach(function (close) {
+//     close.addEventListener('click', function () {
+//         modalChiefThree.style.display = 'none';
+//         modalChiefTwo.style.display = 'none';
+//         modalChiefOne.style.display = 'none';
+//         document.body.removeEventListener('wheel', preventScroll);
+//     });
+// });
+//
+// zoomClickOne.addEventListener('click', function () {
+//     document.body.addEventListener('wheel', preventScroll);
+//     modalChiefOne.style.display = 'block';
+//     modalChiefImg.src;
+//     //captionText.innerHTML = this.alt;
+//
+// });
+//
+// zoomClickTwo.addEventListener('click', function () {
+//     document.body.addEventListener('wheel', preventScroll);
+//     modalChiefTwo.style.display = 'block';
+//     modalChiefImg.src;
+//     //captionText.innerHTML = this.alt;
+//
+// });
+//
+// zoomClickThree.addEventListener('click', function () {
+//     document.body.addEventListener('wheel', preventScroll);
+//     modalChiefThree.style.display = 'block';
+//     modalChiefImg.src;
+//     //captionText.innerHTML = this.alt;
+//
+// });
+//
+// function preventScroll(e) {
+//     e.preventDefault();
+//     e.stopPropagation();
+// }
 // -----------------------------------------------
 
 
